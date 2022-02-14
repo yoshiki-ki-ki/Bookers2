0cb87664859b269d201c8316d1312832441492a9
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-14 20:56:38 +0000

    [Update]18 routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  resources :books, only: [:new, :index, :show]
   root to: "homes#top"
   devise_for :users
+  
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
