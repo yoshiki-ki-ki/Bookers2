b0c7e704ccfecac313e2e1ab6f8c1772f41f3c8b
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-15 06:11:39 +0000

    [Add]ActiveStorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  
+  get 'homes/about', as: 'about'
   resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
 
