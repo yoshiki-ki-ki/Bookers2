016c8b77647f080c7060c8aef5a285843c0ebabc
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-16 13:53:28 +0000

    [Update]book index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/'
   end
 
   def show
   end
   
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   
-  belongs_
+  belongs_to :user
   
 end
