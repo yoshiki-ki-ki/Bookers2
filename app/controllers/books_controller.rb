acdb3dfcda56faebb06a1299dd1799647e14b3ce
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-16 17:53:37 +0000

    [Update]一覧画面　link_toで苦戦中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,13 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to '/'
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
   
   def edit
