f72be1f0ea8122d68935755f25b8667c6ed2c279
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-17 12:01:35 +0000

    [Update]Homeページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
   def index
-  end
 
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about', as: 'about'
   resources :books, only: [:index, :show, :edit, :create]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
