6ff2b7e8fdb73b1d7b8869c4106b95c3880a961b
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-19 09:42:31 +0000

    [Update]infoテンプレート化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = User.find(current_user.id)
   end
 
   def create
@@ -13,7 +14,6 @@ class UsersController < ApplicationController
   def show
     @book = Book.new
     @user = User.find(params[:id])
-    @books = @user.books
   end
 
   def edit
