2ad53e9a012e5bfdbf84c146f1ffb37bb720a69a
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-19 11:42:09 +0000

    [Update]aboutログインしていなくても見れる！

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  get 'homes/about', as: 'about'
+  get 'home/about' => "homes#about", as: 'about'
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :create, :update]
 
