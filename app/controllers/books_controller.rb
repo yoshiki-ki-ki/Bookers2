f0cb6de416720eb5425a6cf32270d09a10567fef
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-20 08:50:32 +0000

    [Add]Bootstrap,FontAwesome

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id)
     else
